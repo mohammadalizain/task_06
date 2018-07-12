fd728b6f284f23b0f7c3d3ae5a445a1762a116b0
mohammad.h.ali@kw.zain.com <mohammad.h.ali@kw.zain.com>
2018-07-12 04:09:45 -0700

    Completed - task6

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Restaurant
+from .forms import RestaurantForm
+from django.shortcuts import redirect
+
 
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
@@ -18,8 +21,16 @@ def restaurant_detail(request, restaurant_id):
     return render(request, 'detail.html', context)
 
 def restaurant_create(request):
+    form = RestaurantForm()
+
+    if request.method =="POST":
+        form =RestaurantForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('restaurant-list')
+
 
     context = {
-
+    "restaurant_form": form
     }
     return render(request, 'create.html', context)
